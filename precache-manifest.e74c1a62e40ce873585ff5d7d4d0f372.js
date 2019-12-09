d8fca8fab3e07c135642f795e27358816c44ef4b
sevenish <evans.samuel7@gmail.com>
2019-12-09 22:44:36 +1030

    Updates

---
+++ precache-manifest.e74c1a62e40ce873585ff5d7d4d0f372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29ec4df0cf7fb12074b37f85673885a",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "36cedb720e7ac1db26f7",
+    "url": "/react-tutorial/static/css/main.62bdaceb.chunk.css"
+  },
+  {
+    "revision": "ff0178b2c039e839d26a",
+    "url": "/react-tutorial/static/js/2.0a2f46cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.0a2f46cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36cedb720e7ac1db26f7",
+    "url": "/react-tutorial/static/js/main.f32b8ba4.chunk.js"
+  },
+  {
+    "revision": "cfb7d8ed1e9eafd4dbdf",
+    "url": "/react-tutorial/static/js/runtime-main.04aa60b1.js"
+  }
+]);
